4a879c26270ce7df3cc858dc61d245719ce9abbf
rohit071 <rv90@live.in>
2015-02-18 17:18:58 -0500

    Added category UI, Insert Time UI and inset image UI

--- public_html/newevent.php
+++ public_html/newevent.php
@@ -1,9 +1,5 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+
 <html>
     <head>
         <meta charset="UTF-8">
@@ -12,13 +8,16 @@ and open the template in the editor.
     <body>
         <?php include 'header.php';?>
         <div id="wrap">
-            <form name="eventadd" onsubmit="">
+            <form name="eventadd" action="upload.php" method="post" enctype="multipart/form-data">
        <table id="addevent">
            <tr>
                <td>Title </td><td><input type="text" name="title" value="" /></td>
            </tr>
            <tr>
                <td>Location </td><td><input type="text" name="location" value="" /></td>
+           </tr>
+           <tr>
+               <td>Event Type </td><td><input type="radio" name="type" value="Public" /> Public&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<input type="radio" name="type" value="Private" /> Private</td>
            </tr>
            <tr>
                <td>Category </td><td><select name="cat" id="">
@@ -32,13 +31,17 @@ and open the template in the editor.
                <td>Date </td><td><input type="text" name="date" value="" /></td>
            </tr>
            <tr>
+               <td>TIme </td><td><input type="time" name="time" value="" /></td>
+           </tr>
+           <tr>
                <td>Description </td><td><textarea name="description" rows="5" cols="40"></textarea></td>
            </tr>
+
            <tr>
-               <td>Pictures </td><td><input type="text" name="picture" value="" /></td>
+               <td>Pictures </td><td>   <input type="file" name="fileToUpload" id="fileToUpload"></td>
            </tr>
            <tr>
-               <td> </td><td><button type="button" class="btn btn-primary">Submit</button>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<button type="button" class="btn btn-primary">Cancel</button></td>
+               <td> </td><td><button type="button" name="submit" value="Upload Image" class="btn btn-primary">Submit</button>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<button type="button" class="btn btn-primary">Cancel</button></td>
            </tr>
        </table>
             </form>
