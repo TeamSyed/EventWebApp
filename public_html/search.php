ee018cafeff955144ca19d9ce02f6b0d4fe7d4ce
rohit kumar <rv90@live.in>
2015-02-23 22:31:03 -0500

    Added Proper Styling
    Added styling to output data. Results has a bug. It is showing only last
    result in database.

--- public_html/search.php
+++ public_html/search.php
@@ -11,7 +11,7 @@
             <div id="top">
                 <h4>Your Search Produced following Results</h4>
             </div>
-            <div class ="middle">
+            
                 <?php 
                 //connect to database 
                 $mysqli = new mysqli('localhost','root','','events');
@@ -36,35 +36,20 @@
                         $pic = $rows['pic'];
                         
                     }
-                    echo"<P>$title$location$type$category$date$time $pic <br/><br/><br/></P>";
+                    echo"<div class='middle'>
+                        <img src='uploads/download.jpg' /><h1>$title</h1><br />
+                <h2>Description of the event. The description of the event will be shown here. This will include the features of the evnts.</h2><br /><br /><h4>Show more></h4>
+                <h3>$location  &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp $date&nbsp&nbsp&nbsp $time  </h3>
+                    </div>
+                    
+                    
+                    
+                    
+                    ";
                 };
                     
                 ?>
-                <img src="uploads/download.jpg" /><h1>Title goes here</h1><br />
-                <h2>Description of the event. The description of the event will be shown here. This will include the features of the evnts.</h2><br />
-                <br /><h4>Show more></h4>
-                <h3>Location  &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp 2015/02/20&nbsp&nbsp&nbsp 06:26 PM  </h3>
-            </div>
-            <div class ="middle">
-                <img src="uploads/download.jpg" /><h1>Title goes here</h1><br />
-                <h2>Description of the event. The description of the event will be shown here. This will include the features of the evnts.</h2><br /><br /><h4>Show more></h4>
-                <h3>Location  &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp 2015/02/20&nbsp&nbsp&nbsp 06:26 PM  </h3>
-            </div>
-            <div class ="middle">
-                <img src="uploads/download.jpg" /><h1>Title goes here</h1><br />
-                <h2>Description of the event. The description of the event will be shown here. This will include the features of the evnts.</h2><br /><br /><h4>Show more></h4>
-                <h3>Location  &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp 2015/02/20&nbsp&nbsp&nbsp 06:26 PM  </h3>
-            </div>
-            <div class ="middle">
-                <img src="uploads/download.jpg" /><h1>Title goes here</h1><br />
-                <h2>Description of the event. The description of the event will be shown here. This will include the features of the evnts.</h2><br /><br /><h4>Show more></h4>
-                <h3>Location  &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp 2015/02/20&nbsp&nbsp&nbsp 06:26 PM  </h3>
-            </div>
-            <div class ="middle">
-                <img src="uploads/download.jpg" /><h1>Title goes here</h1><br />
-                <h2>Description of the event. The description of the event will be shown here. This will include the features of the evnts.</h2><br /><br /><h4>Show more></h4>
-                <h3>Location  &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp 2015/02/20&nbsp&nbsp&nbsp 06:26 PM  </h3>
-            </div>
+                
             <div id="bottom">
                 <img src="Graphics/back.png" /><img src="Graphics/next.png" />
             </div>
