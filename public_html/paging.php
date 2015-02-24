6fdef90823a37c231e81bb1991c408020b2290cf
rohit kumar <rv90@live.in>
2015-02-23 21:49:33 -0500

    Displaying data from database on search page
    data is being displayed from database. Still needed to give styles and
    proper structuring to data.

---
+++ public_html/paging.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php include 'header.php';?>
+       <?php include 'footer.php';?>
+    </body>
+   
+</html>
--- public_html/search.php
+++ public_html/search.php
@@ -12,6 +12,34 @@
                 <h4>Your Search Produced following Results</h4>
             </div>
             <div class ="middle">
+                <?php 
+                //connect to database 
+                $mysqli = new mysqli('localhost','root','','events');
+                
+                
+                //query the database
+                $resultSet = $mysqli->query("SELECT * from  event");
+                
+                
+                //count the returned rows
+                if($resultSet->num_rows !=0){
+                //turn the results into Array
+                    while($rows = $resultSet->fetch_assoc())
+                    {
+                        $title = $rows['title'];
+                        $location = $rows['location'];
+                        $type = $rows['type'];
+                        $category = $rows['category'];
+                        $date = $rows['date'];
+                        $time = $rows['time'];
+                        
+                        $pic = $rows['pic'];
+                        
+                    }
+                    echo"<P>$title$location$type$category$date$time $pic <br/><br/><br/></P>";
+                };
+                    
+                ?>
                 <img src="uploads/download.jpg" /><h1>Title goes here</h1><br />
                 <h2>Description of the event. The description of the event will be shown here. This will include the features of the evnts.</h2><br />
                 <br /><h4>Show more></h4>
