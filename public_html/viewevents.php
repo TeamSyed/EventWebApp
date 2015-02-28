7f9a286eda81390765efb13f67a532bf28495bc9
rrahulsharma91 <rahulsharma2121991@gmail.com>
2015-02-28 20:23:19 +0530

    Load user events on view events page

--- public_html/viewevents.php
+++ public_html/viewevents.php
@@ -1,4 +1,8 @@
-<?php include 'header.php';?>
+<?php
+    include 'header.php';
+    $getEvents = "SELECT * FROM $tbl_events as ev LEFT JOIN $tbl_categories as cat on ev.category_id = cat.id";
+    $getEvents = mysql_query($getEvents);
+?>
         <table border="1" class="viewEvents" align="center">
             <caption><h4>View Events</h4></caption>
             <thead>
@@ -12,17 +16,19 @@
                 </tr>
             </thead>
             <tbody>
+                <?php while($data = mysql_fetch_assoc($getEvents)){?>
                 <tr>
                     <td><img src="uploads/download.jpg" width="50" height="50" alt="EventName"/></td>
-                    <td>title</td>
-                    <td>category</td>
-                    <td>Address</td>
-                    <td>date at time</td>
+                    <td><a href="#" title="View this Event"><?php echo ucwords($data['title']); ?></a></td>
+                    <td><?php echo ucwords($data['name']); ?></td>
+                    <td style="text-align: left;padding: 1px 10px;"><?php echo ucwords($data['address'])."<br />".  ucwords($data['city'])." - ". strtoupper($data['postal_code'])."<br />".  strtoupper($data['province']); ?></td>
+                    <td><?php echo $data['date']; ?> at <?php echo $data['time']; ?></td>
                     <td>
                         <a href="#">Edit</a>
                         <a href="#">Delete</a>
                     </td>
                 </tr>
+                <?php } ?>
             </tbody>
         </table>
 
