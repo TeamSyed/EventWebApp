d72635c3ed55dea32dae273fa2bc1b5eaeeb25db
rrahulsharma91 <rahulsharma2121991@gmail.com>
2015-02-28 17:32:47 +0530

    Load Event Data in Events Page

--- public_html/editevent.php
+++ public_html/editevent.php
@@ -1,70 +1,102 @@
+<?php
+    include_once './header.php';
+    $editId = (isset($_REQUEST['id']))? $_REQUEST['id'] : 10;
+    $query_load = "SELECT * FROM $tbl_events where id = '$editId'";
+    
+    $exe_load = mysql_query($query_load);
+    $edit_data = mysql_fetch_assoc($exe_load);
+    $edit_data['address'] = explode(", ", $edit_data['address']);
+    
+    $$edit_data['address'][2] = "selected";
+    $avenue = (!isset($Road) && !isset($Boulevard)) ? "selected" : "";
+    $$edit_data['province'] = "selected";
+    $$edit_data['type'] = "checked";
+   
+    $qry_cate = "SELECT * FROM $tbl_categories WHERE id =".$edit_data['category_id'];
+    $qry_cate = mysql_query($qry_cate);
+    $qry_cate = mysql_fetch_assoc($qry_cate);
+    $$qry_cate['name'] = "selected";
+    $category_name = explode(" ", $qry_cate['name']);
+    if(count($category_name)>1) $$category_name[1] = "selected";
+?>
 <!DOCTYPE html>
 
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Update Event</title>
+        <title></title>
     </head>
     <body>
-        <?php include 'header.php';?>
+        <br />
         <div id="wrap">
-            <form name="eventadd" method="post" enctype="multipart/form-data" >
+            <form name="eventadd" method="post" enctype="multipart/form-data">
        <table id="addevent">
+           <?php if(isset($message)){  ?>
+           <tr><th colspan="2" style="background: <?php echo $color; ?>;"><?php echo $message; ?></th></tr
+           <?php } ?>
            <tr>
-               <td>Title </td><td class="title"><input id="title" type="text" name="title" value="" placeholder="Enter Title"/><br /><div class="err"></div></td>
+               <td>Title </td><td class="title"><input id="title" type="text" name="title" value="<?php echo $edit_data['title']; ?>" placeholder="Enter Title"maxlength="200"/><br /><div class="err"></div></td>
            </tr>
            <tr>
-               <td>Address </td><td class="address"><input id="address" type="text" name="address" value="" placeholder="Street Number"/><input id="address1" type="text" name="address" value="" placeholder="Street Name Address"/><br /><div class="err"></div></td>
+               <td>Address </td><td class="address">
+                   <div class="street"><input id="street" type="text" name="address[]" value="<?php echo $edit_data['address'][0]; ?>" placeholder="Enter Street Number"/><br /><div class="err"></div></div>
+                   <div class="streetName"><input id="streetName" type="text" name="address[]" value="<?php echo $edit_data['address'][1]; ?>" placeholder="Enter Street Name" onblur="$(this).val($(this).val().capitalize());"/><br /><div class="err"></div>
+                   <select name="address[]">
+                      <option <?php echo (isset($Road)) ? $Road: ""; ?>>Road</option>
+                      <option <?php echo (isset($Boulevard)) ? $Boulevard: ""; ?>>Boulevard</option>
+                      <option <?php echo (isset($avenue)) ? $avenue: ""; ?>>Avenue Street</option>
+                   </select>
+                   <br /><div class="err"></div>
+               </td>
            </tr>
-
            <tr>
-               <td>City </td><td class="city"><input id="city" type="text" name="city" value=""  placeholder="Enter City"/><br /><div class="err"></div></td>
+               <td>City </td><td class="city"><input id="city" type="text" name="city" value="<?php echo $edit_data['city']; ?>"  placeholder="Enter City" maxlength="50" onblur="$(this).val($(this).val().capitalize());"/><br /><div class="err"></div></td>
            </tr>
             <tr>
                <td>Province </td><td class="province"><select name="province" id="province">
                       <option value="">--Select Province--</option>
-                      <option value="ON">Ontario</option>
-                      <option value="QT">Quebec</option>
-                      <option value="NS">Nova Scotia</option>
-                      <option value="NB">New Brunswick</option>
-                      <option value="MB">Manitoba</option>
-                      <option value="BC">British Columbia</option>
-                      <option value="PE">Prince Edward Island</option>
-                      <option value="SK">Saskatchewan</option>
-                      <option value="AB">Alberta</option>
-                      <option value="NL">Newfoundland and Labrador</option>
+                      <option value="ON" <?php echo (isset($ON)) ? $ON: ""; ?>>Ontario</option>
+                      <option value="QT" <?php echo (isset($QT)) ? $QT: ""; ?>>Quebec</option>
+                      <option value="NS" <?php echo (isset($NS)) ? $NS: ""; ?>>Nova Scotia</option>
+                      <option value="NB" <?php echo (isset($NB)) ? $NB: ""; ?>>New Brunswick</option>
+                      <option value="MB" <?php echo (isset($MB)) ? $MB: ""; ?>>Manitoba</option>
+                      <option value="BC" <?php echo (isset($BC)) ? $BC: ""; ?>>British Columbia</option>
+                      <option value="PE" <?php echo (isset($PE)) ? $PE: ""; ?>>Prince Edward Island</option>
+                      <option value="SK" <?php echo (isset($SK)) ? $SK: ""; ?>>Saskatchewan</option>
+                      <option value="AB" <?php echo (isset($AB)) ? $AB: ""; ?>>Alberta</option>
+                      <option value="NL" <?php echo (isset($NL)) ? $NL: ""; ?>>Newfoundland and Labrador</option>
                     </select>  <br /><div class="err"></div>      </td>
            </tr>
            <tr>
-               <td>Postal Code </td><td class="postal_code"><input id="postal_code" type="text" name="postal_code" value="" placeholder="Enter Postal Code"/><br /><div class="err"></div></td>
+               <td>Postal Code </td><td class="postal_code"><input id="postal_code" type="text" name="postal_code" value="<?php echo $edit_data['postal_code']; ?>" placeholder="Enter Postal Code" onkeyup="$(this).val($(this).val().toUpperCase());"/><br /><div class="err"></div></td>
            </tr>
            <tr>
-               <td>Event Type </td><td><input type="radio" name="type" value="Public" checked="checked"/> Public&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<input type="radio" name="type" value="Private" /> Private</td>
+               <td>Event Type </td><td><input type="radio" name="type" value="Public" <?php echo (isset($Public)) ? $Public: ""; ?>/> Public&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<input type="radio" name="type" value="Private" <?php echo (isset($Private)) ? $Private: ""; ?>/> Private</td>
            </tr>
            <tr>
                <td>Category </td><td class="category"><select name="cat" id="category">
                       <option value="">--Select Category--</option>
-                      <option value="1">Cuisine</option>
-                      <option value="2">Job Fair</option>
-                      <option value="3">Job Interview</option>
-                      <option value="4">Traffic Accident</option>
-                      <option value="5">Concert</option>
-                      <option value="6">Meeting</option>
-                      <option value="7">Party</option>
+                      <option value="1" <?php echo (isset($Cuisine)) ? $Cuisine: ""; ?>>Cuisine</option>
+                      <option value="2" <?php echo (isset($Fair)) ? $Fair: ""; ?>>Job Fair</option>
+                      <option value="3" <?php echo (isset($Interview)) ? $Interview: ""; ?>>Job Interview</option>
+                      <option value="4" <?php echo (isset($Accident)) ? $Accident: ""; ?>>Traffic Accident</option>
+                      <option value="5" <?php echo (isset($Concert)) ? $Concert: ""; ?>>Concert</option>
+                      <option value="6" <?php echo (isset($Meeting)) ? $Meeting: ""; ?>>Meeting</option>
+                      <option value="7" <?php echo (isset($Party)) ? $Party: ""; ?>>Party</option>
                     </select>  <br /><div class="err"></div>      </td>
            </tr>
            <tr>
-               <td>Date </td><td class="date"><input type="date" name="date" id="date" value=""  placeholder="Enter Date of Event"/><br /><div class="err"></div></td>
+               <td>Date </td><td class="date"><input  class="dateWidget" type="date" name="date" id="date" value="<?php echo $edit_data['date']; ?>" placeholder="Enter Date of Event"/><br /><div class="err"></div></td>
            </tr>
            <tr>
-               <td>Time </td><td class="time"><input type="time" name="time" value="" id="time"  placeholder="Enter Time for Event"/><br /><div class="err"></div></td>
+               <td>Time </td><td class="time"><input  class="timeWidget" type="time" name="time" value="<?php echo $edit_data['time']; ?>" id="time"  placeholder="Enter Time for Event"/><br /><div class="err"></div></td>
            </tr>
            <tr>
-               <td>Description </td><td class="description"><textarea id="description" name="description" rows="5" cols="40"  placeholder="Describe your event"></textarea> <br /><div class="err"></div></td>
+               <td>Description </td><td class="description"><textarea id="description" name="description" rows="5" cols="40"  placeholder="Describe your event"><?php echo $edit_data['description']; ?></textarea> <br /><div class="err"></div></td>
            </tr>
 
            <tr>
-               <td>Pictures </td><td class="image">   <input type="file" id="image" name="image" ><input type="submit" value="Upload Image" name="upload"> <br /><div class="err"></div>
+               <td>Pictures </td><td class="image">   <input type="file" id="image" name="image" id="image"><input type="submit" value="Upload Image" name="upload"> <br /><div class="err"></div>
                     <?php
                     
 if (isset($_POST['upload'])){
@@ -83,7 +115,7 @@ if (isset($_POST['upload'])){
     if (in_array($extension, $allowed_types, false) != true) {
 
         echo"<script>alert('only JPEG and GIF formats alowed.')</script>";
-        
+        exit();
     }
     
     if ($image_name == ''){
@@ -106,8 +138,7 @@ if (isset($_POST['upload'])){
             </div>
   
         <?php include 'footer.php';?>
-        <div id="img" >  
-              
+        <div id="img" >    
                   </div>
     </body>
    
