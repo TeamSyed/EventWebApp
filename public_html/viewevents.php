581182b8360d10da95bb3f54783a91986b425358
gagan1989 <gagandeepsngh639@gmail.com>
2015-02-28 19:26:18 +0530

    View Events Interface

---
+++ public_html/viewevents.php
@@ -0,0 +1,30 @@
+<?php include 'header.php';?>
+        <table border="1" class="viewEvents" align="center">
+            <caption><h4>View Events</h4></caption>
+            <thead>
+                <tr>
+                    <th>Image</th>
+                    <th>Title</th>
+                    <th>Category</th>
+                    <th>Address</th>
+                    <th>Date/Time</th>
+                    <th>Operations</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td><img src="uploads/download.jpg" width="50" height="50" alt="EventName"/></td>
+                    <td>title</td>
+                    <td>category</td>
+                    <td>Address</td>
+                    <td>date at time</td>
+                    <td>
+                        <a href="#">Edit</a>
+                        <a href="#">Delete</a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+
+<?php include 'footer.php';?>
+   
