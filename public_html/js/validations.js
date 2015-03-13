4372e6b44f57f4cc81fea742056ae721ae566d3b
rohit kumar <rv90@live.in>
2015-03-13 12:46:39 -0400

    Revert "Add Business Event with Style Bar"
    This reverts commit 003f645d1026413c0ea48bb56bb872ea58705f91.

--- public_html/js/validations.js
+++ public_html/js/validations.js
@@ -15,41 +15,50 @@
         
     });
 function validateEventAdd(){
+    
     var numreg = /^[1-9][0-9]*$/;
     
     var regex = /^[A-Za-z]\d[A-Za-z][ -]?\d[A-Za-z]\d$/;
     
     var ids = ['title', 'street', 'streetName', 'city','province', 'time', 'postal_code', 'category', 'description'];
+    var status = true;
     for(i=0;i<ids.length;i++){
         var idVal = $("#"+ids[i]).val();
         if(idVal===""){
             $("."+ids[i]+" .err").html('This field is Required').css({'color':'#f00','font-size':'15px'});
-            return false;
+            status = false;
         } else {
             $("."+ids[i]+" .err").html('');
-             //status = true;
+             status = true;
         }
         if(ids[i] == "street"){
             if(!numreg.test($("#street").val())){ 
                 $(".street .err").html('Numerics Only & not starting with 0').css({'color':'#f00','font-size':'15px'});
-               return false;
+               status = false;
             } else {
                 $(".street .err").html('');
-               //  status = true;
+                 status = true;
             }
         }
         if(ids[i] == "postal_code"){
              if(!regex.test($("#postal_code").val())){ 
                 $(".postal_code .err").html('Invalid Postal Code').css({'color':'#f00','font-size':'15px'});
-                return false;
+                status = false;
             } else{
                 $(".postal_code .err").html('');
-                 //status = true;
+                 status = true;
             }
         }
         
     }
- }
+    if(status){
+        return true;
+       }
+       else
+       {
+           return false;
+       }
+       }
 
 String.prototype.capitalize = function(){
     return this.charAt(0).toUpperCase() + this.slice(1);
--- public_html/viewevents.php
+++ public_html/viewevents.php
@@ -1,6 +1,6 @@
 <?php
     include 'header.php';
-    $getEvents = "SELECT * FROM $tbl_events";
+    $getEvents = "SELECT * FROM $tbl_events as ev LEFT JOIN $tbl_categories as cat on ev.category_id = cat.id";
     $getEvents = mysql_query($getEvents);
 ?>
         <table border="1" class="viewEvents" align="center">
@@ -16,15 +16,11 @@
                 </tr>
             </thead>
             <tbody>
-                <?php while($data = mysql_fetch_assoc($getEvents)){
-                    $getCats = "SELECT name from $tbl_categories where id = '".$data['category_id']."'";
-                    $getCats = mysql_query($getCats);
-                    $getCats = mysql_fetch_assoc($getCats);
-                    ?>
+                <?php while($data = mysql_fetch_assoc($getEvents)){?>
                 <tr>
                     <td><img src="uploads/download.jpg" width="50" height="50" alt="EventName"/></td>
                     <td><a href="eventinfo.php?id=<?php echo $data['id']; ?>" title="View this Event"><?php echo ucwords($data['title']); ?></a></td>
-                    <td><?php echo ucwords($getCats['name']); ?></td>
+                    <td><?php echo ucwords($data['name']); ?></td>
                     <td style="text-align: left;padding: 1px 10px;"><?php echo ucwords($data['address'])."<br />".  ucwords($data['city'])." - ". strtoupper($data['postal_code'])."<br />".  strtoupper($data['province']); ?></td>
                     <td><?php echo $data['date']; ?> at <?php echo $data['time']; ?></td>
                     <td>
