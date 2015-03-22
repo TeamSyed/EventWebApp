65d796f4629ebc478e919c600b0d5f070f76a7e7
agnisen79 <agnisen79@gmail.com>
2015-03-22 07:41:10 -0400

    Current Friend Requests

---
+++ public_html/friendRequests.php
@@ -0,0 +1,40 @@
+﻿<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <link href="css/bootstrap.min.css" rel="stylesheet" />
+    <link href="css/reset.css" type="text/css" rel="stylesheet" />
+    <link href="css/main.css" type="text/css" rel="stylesheet" />
+    <title></title>
+</head>
+<body>
+    <div id="header">
+        <div id="logo">
+            <img src="Graphics/logo.png" />
+        </div>
+        <div id="banner">
+            
+            <img src="Graphics/banner.png" />
+
+        </div>
+        </div>
+<h2> Pending Friend Requests</h2>
+    <table style="width:100%;">
+    <tr>
+        <td>&nbsp;</td>
+        <td>&nbsp;<input id="Submit" type="submit" value="Accept"></td>
+        <td>&nbsp;<input id="Submit" type="submit" value="Decline" /></td>
+        </tr>
+    <tr>
+        <td>&nbsp;</td>
+        <td>&nbsp;<input id="Submit" type="submit" value="Accept"></td>
+        <td>&nbsp;<input id="Submit" type="submit" value="Decline"></td>
+        </tr>
+    <tr>
+        <td>&nbsp;</td>
+        <td>&nbsp;<input id="Submit" type="submit" value="Accept"></td>
+        <td>&nbsp;<input id="Submit" type="submit" value="Decline"></td>
+        </tr>
+    </table>
+
+</body>
+</html>
