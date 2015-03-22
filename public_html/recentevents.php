6fec0439e81d11b11f2be7b66f4519c0bc8e8d89
agnisen79 <agnisen79@gmail.com>
2015-03-22 08:30:37 -0400

    UI to display recent events

---
+++ public_html/recentevents.php
@@ -0,0 +1,37 @@
+﻿<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <link href="css/bootstrap.min.css" rel="stylesheet" />
+    <link href="css/reset.css" type="text/css" rel="stylesheet" />
+    <link href="css/main.css" type="text/css" rel="stylesheet" />
+    <title></title>
+</head>
+<body>
+    <div id="header">
+        <div id="logo">
+            <img src="Graphics/logo.png" />
+        </div>
+        <div id="banner">
+
+            <img src="Graphics/banner.png" />
+
+        </div>
+    </div>
+    <h2> Recent Events</h2>
+    <table style="width:100%;">
+        <tr>
+            <td>&nbsp;</td>
+            <td>&nbsp;<a href="">Click Here</a> to go to event information</td>
+        </tr>
+        <tr>
+            <td>&nbsp;</td>
+            <td>&nbsp;<a href="">Click Here</a> to go to event information</td>
+        </tr>
+        <tr>
+            <td>&nbsp;</td>
+            <td>&nbsp;<a href="">Click Here</a> to go to event information</td>
+        </tr>
+    </table>
+
+</body>
+</html>
