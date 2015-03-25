760570ebdc09ba38beb4630692009e5efff75143
rohit kumar <rv90@live.in>
2015-03-25 10:42:32 -0400

    Coded to load start and end location on the map.

--- public_html/js/map.js
+++ public_html/js/map.js
@@ -6,7 +6,7 @@ var london = new google.maps.LatLng(51.508742, -0.120850);
 function initialize() {
     var mapProp = {
         center: x,
-        zoom: 6,
+        zoom: 5,
         panControl: true,
         zoomControl: true,
         mapTypeControl: true,
@@ -19,19 +19,24 @@ function initialize() {
 
     var map = new google.maps.Map(document.getElementById("gooogleMap"), mapProp);
 
-    var myTrip = [stavanger, amsterdam, london];
+    var myTrip = [stavanger, amsterdam];
     var flightPath = new google.maps.Polyline({
         path: myTrip,
         strokeColor: "#0000FF",
         strokeOpacity: 0.8,
         strokeWeight: 2
     });
-    var marker=new google.maps.Marker({
+    var markerStart=new google.maps.Marker({
         position:stavanger,
         animation:google.maps.Animation.BOUNCE
     });
+    var markerEnd = new google.maps.Marker({
+        position: amsterdam,
+        animation: google.maps.Animation.BOUNCE
+    });
 
-    marker.setMap(map);
+    markerStart.setMap(map);
+    markerEnd.setMap(map);
 
 
     flightPath.setMap(map);
