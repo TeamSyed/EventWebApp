b658519e83a46dae24c7272a6139a7636ccc52bd
rrahulsharma91 <rahulsharma2121991@gmail.com>
2015-04-07 21:21:40 +0530

    #91  Code home page to load shared events

---
+++ public_html/sharedevents.php
@@ -0,0 +1,34 @@
+<?php include_once 'header.php' ?> 
+<table id="view"  align="center">
+            <caption><h4>Shared Events</h4></caption>
+            <thead>
+                <tr>
+                    <th>Image</th>
+                    <th>Title</th>
+                    <th>Category</th>
+                    <th>Address</th>
+                    <th>Date/Time</th>
+                    <th>Operations</th>
+                    <th>View On Map</th>
+                </tr>
+            </thead>
+            <tbody>
+               
+                <tr>
+                   <td>Image</td>
+                   <td>Event Title</td>
+                   <td>Category Name</td>
+                   <td>Full Address</td>
+                   <td>Date and Time when event has to occur</td>
+                   <td>Edit/ Delete Events</td>
+                   <td>Link to Google Map</td>
+                </tr>
+                 <tr>
+ <td colspan="7" style='text-align: center'>--Pagination--
+ </td>
+ </tr>
+            </tbody>
+        </table>
+
+<?php include 'footer.php';?>
+   
