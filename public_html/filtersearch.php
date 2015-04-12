a0794fbd4c248e6c08a74f763fbc53730f933662
agnisen79 <agnisen79@gmail.com>
2015-04-12 00:19:33 -0400

    UI for filtering events

---
+++ public_html/filtersearch.php
@@ -0,0 +1,22 @@
+﻿
+
+$(document).ready(function(){
+    $("#flip").click(function(){
+        $("#asearch").slideDown("slow");
+    });
+});
+$(document).ready(function () {
+    $("#cancel").click(function () {
+        $("#asearch").slideUp("slow");
+    });
+ });
+$(document).ready(function () {
+ $("#openDir").click(function(){
+        $("#loadAddress").slideDown("slow");
+    });
+});
+$(document).ready(function () {
+    $("#cancelAddress").click(function () {
+        $("#loadAddress").slideUp("slow");
+    });
+});
