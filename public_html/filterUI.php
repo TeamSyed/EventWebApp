d46e9150daf280e673d69466ef0f71b5c602a6d7
agnisen79 <agnisen79@gmail.com>
2015-04-12 00:19:33 -0400

    UI for filtering events

---
+++ public_html/filterUI.php
@@ -0,0 +1,23 @@
+﻿<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <link href="css/bootstrap.min.css" rel="stylesheet" />
+    <link href="css/reset.css" type="text/css" rel="stylesheet" />
+    <link href="css/main.css" type="text/css" rel="stylesheet" />
+    <title></title>
+</head>
+<body>
+      <div id="header">
+            <div id="logo">
+                        <img src="Graphics/logo.png" />
+            
+        </div>
+        <div id="banner">
+       <img src="Graphics/banner.png" />
+            
+        </div>
+    </div>
+    <input id="Submit1" type="submit" value="filter" style="float: right;"  />
+</body>
+</html>
+   
